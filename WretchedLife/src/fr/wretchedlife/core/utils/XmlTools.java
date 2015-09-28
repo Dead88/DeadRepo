931f62042cbb35be2f2aa7531ca5f8d79270290d
Dead88 <Dead@Dead.fr>
2015-09-29 01:47:06 +0200

    another commit

---
+++ WretchedLife/src/fr/wretchedlife/core/utils/XmlTools.java
@@ -0,0 +1,14 @@
+package fr.wretchedlife.core.utils;
+
+public class XmlTools {
+	
+	public String getXMLStringFromObject( Object o ) {
+		
+		return null;
+	}
+	
+	public Object getObjectFromXMLString( String s ) {
+		
+		return null;
+	}
+}
